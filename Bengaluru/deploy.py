c17a49d2356e2e200e70dd5ea6269ce0084e748f
Quoc Trung Huynh <62810853+Chunporo@users.noreply.github.com>
2024-05-21 15:09:00 +0700

    create 16 files and update 1 file

---
+++ Bengaluru/deploy.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+import pickle 
+
+with open('rf_model.h5','rb') as f:
+    saved_model = pickle.load(f)
+
+st.header("House Price Prediction",divider = 'rainbow')    
+LotArea = st.number_input("Insert lot size in square feet", value=0)
+YearBuilt = st.number_input("Insert original construction date",value = 0)
+a1stFlrSF = st.number_input("Insert first floor square feet", value = 0)
+a2ndFlrSF = st.number_input("Insert second floor square feet", value = 0)
+FullBath = st.number_input("Insert full bathrooms above grade", value = 0)
+BedroomAbvGr = st.number_input("Insert bedrooms above grade ", value = 0)
+TotRmsAbvGrd = st.number_input("Insert total rooms above grade",value = 0)
+
+th
+features = [LotArea,YearBuilt,a1stFlrSF,a2ndFlrSF,FullBath,BedroomAbvGr,TotRmsAbvGrd]
+if st.button('Predict',type="primary"):
+    st.write('Price',saved_model.predict([features])[0],'USD')
